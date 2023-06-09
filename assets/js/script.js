1294a883ef4677479242e988824a0dee799684cc
Nawang Yolmo <97735450+designtribestudio@users.noreply.github.com>
2023-06-09 15:17:38 +0530

    dynamic done

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,13 +11,32 @@
 
 // window.addEventListener("scroll", myScrollFunc);
 
-var card1 = document.getElementById('card1');
-var card1icon = document.getElementById('card1icon');
-var card1arrow = document.getElementById('card1iarrow');
-var card2 = document.getElementById('card2');
-var card2icon = document.getElementById('card2icon');
-var card2arrow = document.getElementById('card2iarrow');
-var card3 = document.getElementById('card3');
-var card3icon = document.getElementById('card3icon');
-var card3arrow = document.getElementById('card3iarrow');
+var card1 = document.getElementById("card1");
+var card1icon = document.getElementById("card1icon");
+var card1arrow = document.getElementById("card1iarrow");
+var card2 = document.getElementById("card2");
+var card2icon = document.getElementById("card2icon");
+var card2arrow = document.getElementById("card2iarrow");
+var card3 = document.getElementById("card3");
+var card3icon = document.getElementById("card3icon");
+var card3arrow = document.getElementById("card3iarrow");
 
+var powercard = document.getElementById("powercard");
+var powerbtns = header.getElementsByClassName("power-btn");
+for (var i = 0; i < powerbtns.length; i++) {
+  powerbtns[i].addEventListener("click", function () {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+
+var dynamic = document.getElementById("dynamic");
+var dc = dynamic.getElementsByClassName("dc");
+for (var i = 0; i < powerbtns.length; i++) {
+  powerbtns[i].addEventListener("click", function () {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
