8a63521851b0c173403d89bc701425d105665ef0
Nawang Yolmo <97735450+designtribestudio@users.noreply.github.com>
2023-06-09 11:51:36 +0530

    cards hover transition

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,23 @@
-mList = document.getElementById("mList");
+// mList = document.getElementById("mList");
 
-var myScrollFunc = function() {
-  var y = window.scrollY;
-  if (y >= 3400) {
-    mList.style.display='block';
-  } else {
-    mList.style.display='none';
-  }
-};
+// var myScrollFunc = function() {
+//   var y = window.scrollY;
+//   if (y >= 3400) {
+//     mList.style.display='block';
+//   } else {
+//     mList.style.display='none';
+//   }
+// };
 
-window.addEventListener("scroll", myScrollFunc);
+// window.addEventListener("scroll", myScrollFunc);
+
+var card1 = document.getElementById('card1');
+var card1icon = document.getElementById('card1icon');
+var card1arrow = document.getElementById('card1iarrow');
+var card2 = document.getElementById('card2');
+var card2icon = document.getElementById('card2icon');
+var card2arrow = document.getElementById('card2iarrow');
+var card3 = document.getElementById('card3');
+var card3icon = document.getElementById('card3icon');
+var card3arrow = document.getElementById('card3iarrow');
+
